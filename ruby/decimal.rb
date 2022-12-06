1abc8cb4e1685fb422dc56a0f051f7b6f72916fb
Zeke Barefoot <syncorex0@gmail.com>
2022-12-06 11:10:47 -0700

    var rename

--- ruby/decimal.rb
+++ ruby/decimal.rb
@@ -5,8 +5,8 @@ ok = ["1.0","-1.0","0.0","329'124.038238"]
 not_ok = ["001.0","123..123","22'.123","123.123.32"]
 
 puts "ok"
-ok.each { |addr| raise "#{addr} should match decimal" unless (addr =~ decimal) }
+ok.each { |number| raise "#{number} should match decimal" unless (number =~ decimal) }
 
 puts "not_ok"
-not_ok.each { |addr| raise "#{addr} should not match decimal" unless not (addr =~ decimal) }
+not_ok.each { |number| raise "#{number} should not match decimal" unless not (number =~ decimal) }
 
